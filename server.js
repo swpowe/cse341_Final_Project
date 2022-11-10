f682dfd82fe532607160e51f432c4bef4ca944be
swpowe <spencer.powell@me.com>
2022-11-09 19:13:15 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// **users**
+// GET /all-users
+
+// GET /user/:id
+
+// POST /user
+
+// PUT /user/:id
+
+// DELETE /delete-user/:id
+
+// **lists**
+// GET / all-lists
+
+// GET /list/:id
+
+// POST /list
+
+// PUT /list/:id
+
+// DELETE /list/:id
+
+// **items**
+// GET /all-items
+
+// GET /item/:id
+
+// GET /all-items-label/:id
+
+// PUT /item/:id
+
+// DELETE /item/:id
+
+// **labels**
+// PUT /label/:id
