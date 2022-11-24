d79e3f4fa166bc45905ecf6286993548fc536ee5
swpowe <spencer.powell@me.com>
2022-11-23 19:46:04 -0700

    working on testing with jest broken

--- config/db.js
+++ config/db.js
@@ -5,9 +5,11 @@ const connectDB = async () => {
     try {
         const connect = await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
+        return 0;
     } catch (err) {
         console.log('Cannot connect to Database');
-        process.exit(1);
+        // process.exit(1);
+        return 1;
     }
 }
 
