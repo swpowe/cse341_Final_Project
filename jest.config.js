c1a211430e91d23ac8a49876e50353abc8a6c415
niiquash <qua16006@byui.edu>
2022-12-09 15:28:36 -0700

    completed tests on files in the routes folder. All tests pass.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
-module.exports = {
-    testEnvironment: 'node',
-    setupFiles: ["<rootDir>/.jest/setEnvVars.js"]
-  };
+// module.exports = {
+//     testEnvironment: 'node',
+//     setupFiles: ["<rootDir>/.jest/setEnvVars.js"]
+//   };
+
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+};
+
+module.exports = config;
---
+++ routes/__tests__/todos.test.js
@@ -0,0 +1,31 @@
+const todosRouter = require("../todos");
+const routesFor = require("../testHelperFunction/routesFor");
+
+const routes = routesFor(todosRouter);
+
+describe("routes", () => {
+    describe("/", () => {
+        it("supports http GET", () => {
+            expect(routes["/"]).toContain("get");
+        });
+
+        it("supports http POST", () => {
+            expect(routes["/"]).toContain("post");
+        });
+    });
+
+    describe("/:id", () => {
+        it("supports http GET", () => {
+            expect(routes["/:id"]).toContain("get");
+        });
+
+        it("supports http PUT", () => {
+            expect(routes["/:id"]).toContain("put");
+        });
+        it("supports http DELETE", () => {
+            expect(routes["/:id"]).toContain("delete");
+        });
+
+    })
+})
+
